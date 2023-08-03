9a7472d3fb4aa02e34b87b1d51ec50b6ef7cd0d3
TracyAntonia <aantonjuguna@gmail.com>
2023-08-03 23:43:28 +0300

    updated files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 const searchApi = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
 function App() {
-  
+  const [isLoading, setIsLoading] = useState(false);
+  const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+
+  //searching for recipes
+  const searchRecipes = async () => {
+    setIsLoading(true);
+    const url = searchApi + query
+    const res = await fetch(url);
+    const data = await res.json();
+    setRecipes(data.meals);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    searchRecipes()
+  }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    searchRecipes();
+  }
+
+  return (
+    <div className="container">
+      <h2>Our Food Recipes</h2>
+     </div>
+  )
 }
 
 export default App
